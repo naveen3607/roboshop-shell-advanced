3acd056f776678fbd4e4b7de1d51aea2e28411ce
naveen3607 <naveenreddykallam3607@gmail.com>
2023-08-11 18:03:55 -0400

    Shell Script

--- common.sh
+++ common.sh
@@ -110,7 +110,7 @@ func_systemd (){
   #Reload, enable & start the service
   echo -e "\e[36m>>>>>>>>>>> Starting ${component} service <<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
   systemctl daemon-reload
-  systemctl enable ${component} &>>/tmp/roboshop.log
+  systemctl enable ${component}
   systemctl restart ${component} &>>/tmp/roboshop.log
   func_exit_status
 }
